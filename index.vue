e1717a88130353769a9e8cb13e7a1f605dd0f8d6
caner <5658514@qq.com>
2022-10-28 09:44:07 +0800

    Signed-off-by: caner <5658514@qq.com>

---
+++ index.vue
@@ -0,0 +1,188 @@
+<template>
+  <div class="videoBox">
+    <video
+      id="test"
+      crossorigin="anonymous"
+      autoplay
+      playsinline
+      muted
+      controls
+    />
+    <div class="contrl">
+      <button @click="contrl(8)">
+        上
+      </button>
+      <button @click="contrl(2)">
+        下
+      </button>
+      <button @click="contrl(4)">
+        左
+      </button>
+      <button @click="contrl(6)">
+        右
+      </button>
+      <button @click="contrl(10)">
+        放大
+      </button>
+      <button @click="contrl(11)">
+        缩小
+      </button>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import axios from 'axios'
+import { onMounted, onUnmounted, ref } from 'vue'
+let webRtc:any = null
+const cUUID = ref('')
+const TOKEN = '833cd2ff8851509d0c4b2cab0dbdb74d'
+/**
+ * rtc 初始化
+ * @param HOST
+ * @param token
+ * @param uuid
+ * @param callBack
+ * @param profile 0 主 1 子
+ */
+const initWebRtc = async (HOST:String, token:String, uuid:string, callBack:Function, profile = 1) => {
+  try {
+  // init RTC
+    const Peer = new RTCPeerConnection() as any
+    // Peer.addTransceiver('audio', { direction: 'recvonly' })
+    Peer.addTransceiver('video', { direction: 'recvonly' })
+    const offer = await Peer.createOffer()
+    await Peer.setLocalDescription(offer)
+    // 监听视频
+    Peer.ontrack = callBack
+
+    Peer.oniceconnectionstatechange = () => {
+      const state = Peer.iceConnectionState
+      console.log('ICE状态', state)
+    }
+
+    Peer.onicegatheringstatechange = () => {
+      console.log('GatheringState: ', Peer.iceGatheringState)
+    }
+
+    Peer.onconnectionstatechange = () => {
+      const state = Peer.connectionState
+      console.log('连接状态', state)
+    }
+    // SDP SRS params
+    const params = {
+      api: `http://${HOST}:1985/rtc/v1/play/?token=${token}&uuid=${uuid}&stream=${uuid + profile}&profile=${profile}`,
+      clientip: null,
+      sdp: offer.sdp,
+      streamurl: `webrtc://${HOST}/live/${uuid + profile}?token=${token}&uuid=${uuid}&stream=${uuid + profile}&profile=${profile}`,
+      tid: Number(Math.floor(new Date().getTime() * Math.random() * 100)).toString(16).slice(0, 7)
+    }
+    // 发送offer
+    const res = await window.fetch(params.api, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(params)
+    })
+
+    // 接收 answer
+    const { sdp } = await res.json()
+    if (sdp) await Peer.setRemoteDescription(new RTCSessionDescription({ type: 'answer', sdp }))
+
+    return Peer
+  } catch (error) {
+    return null
+  }
+}
+
+/**
+ * 云台控制
+ * @param url
+ * @param params
+ */
+const camerContrl = async (url:string, params:object) => {
+  // 发送offer
+  const respons = await window.fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(params)
+  })
+  const res = await respons.json()
+  return res
+}
+
+// 获取摄像头播放地址
+const getCamer = async () => {
+  const { data } = await axios.post('http://20.20.20.9:8089', {
+    code: 'categorycamera.list',
+    token: TOKEN,
+    body: {}
+  })
+  // 35可控
+  const tmp = data.filter((el:{name:string}) => (el.name.includes('35')))
+  // 默认测试第一个
+  console.log('摄像头列表', tmp)
+  cUUID.value = tmp[2].uuid || ''
+  if (webRtc) webRtc.close()
+  webRtc = await initWebRtc('20.20.20.9', TOKEN, cUUID.value, (event:any) => {
+    const dom = document.getElementById('test') as any
+    const { streams } = event
+    dom.srcObject = streams[0]
+    console.log('track', event)
+  })
+}
+
+// 云台控制
+const contrl = (command:number) => {
+  if (!cUUID.value) return
+  const params = {
+    code: 'cloudcontrol.control',
+    token: TOKEN,
+    body: {
+      uuid: cUUID.value, // 摄像头id
+      command, // 指令:数字键盘1-9去掉5,10:焦距放大,11:焦距缩小
+      // 12:亮度,13:色彩饱和度,14:对比度,15:清晰度,
+      number: 1 // [云台速度|焦距参数|色彩饱和度]等值  亮度值 0-100
+    }
+  }
+  camerContrl('http://20.20.20.9:8089', params)
+}
+
+// 获取目录
+// const proList = async () => {
+//   const { data } = await axios.post('http://20.20.20.9:8089', {
+//     code: 'category.list',
+//     token: '833cd2ff8851509d0c4b2cab0dbdb74d',
+//     body: {}
+//   })
+//   // 零食取一个测试
+//   console.log(666, data)
+// }
+
+onMounted(() => {
+  getCamer()
+})
+
+onUnmounted(() => {
+  if (webRtc) webRtc.close()
+})
+</script>
+<style lang="less" scoped>
+.videoBox{
+  video{
+    width: 800px;
+    height: 500px;
+    background: none;
+    object-fit: fill;
+    font-size: 0;
+    display: block;
+    margin: 0 auto;
+  }
+  .contrl{
+    margin-top: 10px;
+    text-align: center;
+    button{
+      margin-left: 10px;
+    }
+  }
+}
+
+</style>
